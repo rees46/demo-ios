44b34bb4cba0bc94ae5d973cc673734891e9b72b
Daniel Green <gdl@rees46.ru>
2024-07-03 12:49:29 +0200

    Initial Commit

---
+++ demo-store-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  demo-store-ios
+//
+//  Created by Daniel Green on 2024-07-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ demo-store-ios/demo_store_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  demo_store_iosApp.swift
+//  demo-store-ios
+//
+//  Created by Daniel Green on 2024-07-03.
+//
+
+import SwiftUI
+
+@main
+struct demo_store_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
