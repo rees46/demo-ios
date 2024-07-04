8a66118629d25ed98e6a9ffe9b38d9ec7f2b7213
Daniel Green <gdl@rees46.ru>
2024-07-04 17:29:20 +0200

    feat: Add toolbar view and icons

--- demo-store-ios/features/splash/ContentView.swift
+++ demo-store-ios/features/splash/ContentView.swift
@@ -3,29 +3,73 @@ import SwiftUI
 struct ContentView: View {
     
     var body: some View {
-        CustomTabView()
-            .edgesIgnoringSafeArea(.all)
+        VStack(spacing: 0) {
+            ToolbarView()
+            
+            Divider()
+            
+            BottomBarView()
+                .edgesIgnoringSafeArea(.bottom)
+        }
+        .background(Color.white.edgesIgnoringSafeArea(.all))
     }
 }
 
-struct CustomTabView: View {
+struct ToolbarView: View {
+    var body: some View {
+        VStack {
+            ZStack {
+                Color.black
+                Image("ToolbarLogo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .padding(.vertical, 10)
+                    .foregroundColor(.white)
+            }
+            .frame(height: 50)
+            
+            HStack(spacing: 20) {
+                
+                Image("MenuIcon")
+                    .resizable()
+                    .renderingMode(.template)
+                    .frame(width: 24, height: 24)
+                    .foregroundColor(.black)
+                    .padding(.leading, 12)
+                
+                Spacer()
+                
+                Image("SearchIcon")
+                    .resizable()
+                    .renderingMode(.template)
+                    .frame(width: 30, height: 30)
+                    .foregroundColor(.black)
+                
+                Image("CartIcon")
+                    .resizable()
+                    .renderingMode(.template)
+                    .frame(width: 30, height: 30)
+                    .foregroundColor(.black)
+                    .padding(.trailing, 12)
+            }
+            .frame(height: 50)
+        }
+        .background(Color.white)
+    }
+}
+
+struct BottomBarView: View {
     @State private var selectedTab = 0
     
     var body: some View {
         
         VStack {
-            // Content
             switch selectedTab {
-            case 0:
-                MainScreenView()
-            case 1:
-                CatalogScreenView()
-            case 2:
-                CartScreenView()
-            case 3:
-                SettingsScreenView()
-            default:
-                MainScreenView()
+            case 0:MainScreenView()
+            case 1:CatalogScreenView()
+            case 2:CartScreenView()
+            case 3:SettingsScreenView()
+            default:MainScreenView()
             }
             
             Spacer()
