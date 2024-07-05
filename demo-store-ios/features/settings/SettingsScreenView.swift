295e18b9b80ed1102a94b6612f12a8be8e33dd61
Daniel Green <gdl@rees46.ru>
2024-07-05 14:43:29 +0200

    feat: Add settings screen UI

--- demo-store-ios/features/settings/SettingsScreenView.swift
+++ demo-store-ios/features/settings/SettingsScreenView.swift
@@ -1,15 +1,57 @@
-
 import SwiftUI
 
 struct SettingsScreenView: View {
+    @State private var username: String = ""
+    
     var body: some View {
         NavigationView {
-            VStack {
-                Text("settings_tab_title")
-                    .foregroundColor(.white)
+            VStack(spacing: 20) {
+                Text("settings_title")
+                    .font(.title)
+                    .font(.system(size: 24))
+                    .foregroundColor(.black)
+                
+                Text("settings_sub_title")
+                    .font(.system(size: 18))
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.black)
+                
+                VStack {
+                    TextField("store_key_hint", text: $username)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .font(.system(size: 16))
+                        .frame(height: 55)
+//                        .padding(.horizontal)
+                    
+                    Button(action: {
+                        // Action to perform when the button is tapped
+                        print("Button tapped")
+                    }) {
+                        Text("upload_button_title")
+                            .foregroundColor(.white)
+                            .padding()
+                            .frame(height: 44)
+                            .font(.system(size: 16))
+                            .frame(maxWidth: .infinity)
+                            .background(Color.black)
+                            .cornerRadius(10)
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                }
+                
+                Text("process_timeout_title")
+                    .font(.system(size: 13))
+                    .foregroundColor(.gray)
             }
+            .padding()
             .navigationTitle("settings_tab_title")
-            .background(Color.black.edgesIgnoringSafeArea(.all))
+            .background(Color.white.edgesIgnoringSafeArea(.all))
         }
+    }
+}
+
+struct SettingsScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsScreenView()
     }
 }
