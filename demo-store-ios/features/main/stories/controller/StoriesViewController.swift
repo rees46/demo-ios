70b1d1f3fce5187fd6b5eb1f043bfcfd477986c4
Daniel Green <gdl@rees46.ru>
2024-07-09 11:33:02 +0200

    feat: Change stories code

--- demo-store-ios/features/main/stories/controller/StoriesViewController.swift
+++ demo-store-ios/features/main/stories/controller/StoriesViewController.swift
@@ -3,13 +3,14 @@ import UIKit
 import REES46
 
 class StoriesViewController: UIViewController {
+    
     var sdk: PersonalizationSDK?
     var storiesView: StoriesView?
+    var storiesCode:String = "40e9f2c0b33aa016dba02b5b1024bd36"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .clear
-        print("StoriesViewController loaded")
     }
     
     func configure(with sdk: PersonalizationSDK) {
@@ -26,23 +27,21 @@ class StoriesViewController: UIViewController {
             storiesView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
         
-        storiesView.configure(sdk: sdk, mainVC: self, code: "40e9f2c0b33aa016dba02b5b1024bd36")
-        print("StoriesView configured with SDK")
+        storiesView.configure(sdk: sdk, mainVC: self, code: storiesCode)
     }
 }
 
 struct StoriesViewControllerRepresentable: UIViewControllerRepresentable {
+    
     @EnvironmentObject var sdkViewModel: SDKViewModel
     
     func makeUIViewController(context: Context) -> StoriesViewController {
         let viewController = StoriesViewController()
-        print("StoriesViewControllerRepresentable: Created StoriesViewController")
         return viewController
     }
     
     func updateUIViewController(_ uiViewController: StoriesViewController, context: Context) {
         if let sdk = sdkViewModel.sdkManager.sdk {
-            print("StoriesViewControllerRepresentable: SDK found, configuring StoriesView")
             uiViewController.configure(with: sdk)
         } else {
             print("StoriesViewControllerRepresentable: SDK not found")
--- demo-store-ios/viewModel/SdkViewModel.swift
+++ demo-store-ios/viewModel/SdkViewModel.swift
@@ -2,15 +2,4 @@ import SwiftUI
 
 class SDKViewModel: ObservableObject {
     let sdkManager = SDKManager.shared
-    
-    func performSDKAction() {
-        // Use sdkManager.sdk to perform actions
-        if let sdk = sdkManager.sdk {
-            // Example of using sdk
-        }
-    }
-    
-    func loadStories() {
-           // Perform actions to load stories if needed
-       }
 }
