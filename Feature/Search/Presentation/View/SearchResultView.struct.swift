4cd1d5264422d2f2e5de9d3eee88e114d25c9979
Daniel Green <gdl@rees46.ru>
2024-08-09 14:22:22 +0200

    refactor(search): Separate search result view

--- Feature/Search/Presentation/View/SearchResultView.struct.swift
+++ Feature/Search/Presentation/View/SearchResultView.struct.swift
@@ -9,84 +9,15 @@ struct SearchResultView: View {
     
     var body: some View {
         VStack(alignment: .leading) {
-            
             Spacer().frame(height: Sizes.Size.commonHeight)
             
-            HStack {
-                Button(
-                    action: {
-                        navigationManager.navigateBack()
-                    }
-                ) {
-                    Image(systemName: "chevron.left")
-                        .foregroundColor(AppColors.colorBlack)
-                }
-                .padding(.leading, Sizes.Padding.standard)
-                
-                Text("search_result_title")
-                    .font(.system(size: Typography.FontSize.xxxLarge, weight: .bold))
-                    .foregroundColor(AppColors.colorBlack)
-                    .padding(.horizontal)
-                
-                Spacer()
-                
-                Button(
-                    action: {
-                        // TODO: Route to Filter screen
-                    }
-                ) {
-                    Image(systemName: "slider.horizontal.3")
-                        .foregroundColor(AppColors.colorBlack)
-                }
-                .padding(.trailing, Sizes.Padding.standard)
-                
-                Button(
-                    action: {
-                        navigationManager.navigateToRootScreen(
-                            screen: ScreenWrapper(
-                                screen: HomeScreenView()
-                            ),
-                            selectedTab: .home
-                        )
-                    }
-                ) {
-                    Image(systemName: "xmark")
-                        .foregroundColor(AppColors.colorBlack)
-                }
-                .padding(.trailing, Sizes.Padding.standard)
-            }
-            .frame(height: Sizes.Size.commonHeight)
-            .background(AppColors.colorWhite)
+            SearchResultHeaderView(navigationManager: _navigationManager)
             
-            HStack {
-                Text("search_found_title")
-                    .font(.system(size: Typography.FontSize.medium))
-                    .foregroundColor(AppColors.colorBlack)
-                
-                Text(String(format: NSLocalizedString("search_count_result", comment: ""), "\(count)"))
-                    .font(.system(size: Typography.FontSize.medium))
-                    .foregroundColor(AppColors.colorGray)
-            }
-            .padding(.vertical, Sizes.Padding.medium)
-            .background(AppColors.colorWhite)
-            .padding(.horizontal)
+            SearchResultInfoView(count: count)
             
-            ScrollView {
-                LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())], spacing: 16) {
-                    ForEach(recommendedProducts, id: \.id) { product in
-                        ProductListItemView(
-                            product: product,
-                            containerWidth: Sizes.Size.largeImage,
-                            containerHeight: Sizes.Size.largeImageWidth,
-                            imageWidth: Sizes.Size.largeImage,
-                            imageHeight: Sizes.Size.largeImage,
-                            showShopButton: true
-                        ).padding(Sizes.Padding.xxLarge)
-                    }
-                }
+            SearchResultProductGridView(recommendedProducts: recommendedProducts)
                 .padding(.horizontal)
                 .padding(.bottom, Sizes.Padding.xxLarge)
-            }
         }
         .background(AppColors.colorWhite)
         .edgesIgnoringSafeArea(.all)
