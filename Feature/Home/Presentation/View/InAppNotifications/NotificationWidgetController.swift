900081483d1226d2e8abc9af9d8959cef75bdbe1
Daniel Green <gdl@rees46.ru>
2024-12-10 15:36:09 +0200

    feat: Update app version and distributed on app store testing

---
+++ Feature/Home/Presentation/View/InAppNotifications/NotificationWidgetController.swift
@@ -0,0 +1,50 @@
+import SwiftUI
+import UIKit
+import REES46
+
+class NotificationWidgetController: ObservableObject {
+    private var widget: NotificationWidget?
+    
+    func setViewController(_ viewController: UIViewController) {
+        widget = NotificationWidget(parentViewController: viewController)
+    }
+    
+    func showAlert() {
+        widget?.showAlert(
+            titleText: NSLocalizedString("notification_alert_title", comment: ""),
+            messageText: NSLocalizedString("notification_alert_message", comment: ""),
+            imageUrl: AppConfigVariables.inAppNotificationImageUrl,
+            positiveButtonText: NSLocalizedString("notification_accept_button", comment: ""),
+            negativeButtonText: NSLocalizedString("notification_decline_button", comment: ""),
+            onPositiveButtonClick: {
+                print("Alert Accepted")
+            }
+        )
+    }
+    
+    func showBottomSheet() {
+        widget?.showBottomSheet(
+            titleText: NSLocalizedString("notification_alert_title", comment: ""),
+            messageText: NSLocalizedString("notification_alert_message", comment: ""),
+            imageUrl: AppConfigVariables.inAppNotificationImageUrl,
+            positiveButtonText: NSLocalizedString("notification_accept_button", comment: ""),
+            negativeButtonText: NSLocalizedString("notification_decline_button", comment: ""),
+            onPositiveButtonClick: {
+                print("Bottom Sheet Accepted")
+            }
+        )
+    }
+    
+    func showFullScreenAlert() {
+        widget?.showFullScreenAlert(
+            titleText: NSLocalizedString("notification_alert_title", comment: ""),
+            messageText: NSLocalizedString("notification_alert_message", comment: ""),
+            imageUrl: AppConfigVariables.inAppNotificationImageUrl,
+            positiveButtonText: NSLocalizedString("notification_accept_button", comment: ""),
+            negativeButtonText: NSLocalizedString("notification_decline_button", comment: ""),
+            onPositiveButtonClick: {
+                print("Full Screen Alert Accepted")
+            }
+        )
+    }
+}
